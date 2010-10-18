799088b47f9cc8b97c6bcceb98fbb8dcbd51ef26
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2010-10-18 14:28:08 +0000

    Added doc && headers to ocaml-flac and ocaml-aacplus
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407831

--- src/flac.ml
+++ src/flac.ml
@@ -1,7 +1,26 @@
+(*
+ * Copyright 2003-2010 Savonet team
+ *
+ * This file is part of Ocaml-flac.
+ *
+ * Ocaml-flac is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocaml-flac is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ocaml-flac; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
+
+(* Author; Romain Beauxis <toots@rastageeks.org> *)
 
 exception Internal
-
-exception Not_flac
 
 external init : unit -> unit = "ocaml_flac_stubs_initialize"
 
@@ -57,6 +76,7 @@ struct
   exception Bad_header
   exception Frame_crc_mismatch
   exception Unparseable_stream
+  exception Not_flac
 
   let () =
     Callback.register_exception "flac_dec_exn_lost_sync" Lost_sync;
--- src/ogg_flac.ml
+++ src/ogg_flac.ml
@@ -1,3 +1,24 @@
+(*
+ * Copyright 2003-2010 Savonet team
+ *
+ * This file is part of Ocaml-flac.
+ *
+ * Ocaml-flac is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocaml-flac is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ocaml-flac; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
+
+(* Author; Romain Beauxis <toots@rastageeks.org> *)
 
 module Decoder = 
 struct
